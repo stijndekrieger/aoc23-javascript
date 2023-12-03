a58e03476e3441d447c062ae48b980126fc243ba
stijndekrieger <stijndekrieger@gmail.com>
2023-12-03 14:05:55 +0100

    start day 02

---
+++ day02/day02.js
@@ -0,0 +1,5 @@
+import { part1 } from './parts/part1.js';
+
+const filePath = 'day02/data/games.txt';
+
+console.log("Part 1: " + part1(filePath));
---
+++ day02/parts/part1.js
@@ -0,0 +1,3 @@
+export function part1(filePath) {
+    return 0;
+}
