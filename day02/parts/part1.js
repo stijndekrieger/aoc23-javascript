42a36c5871966f0582f0d18354cb01801788fb11
stijndekrieger <stijndekrieger@gmail.com>
2023-12-03 14:17:35 +0100

    start on part 1

--- day02/parts/part1.js
+++ day02/parts/part1.js
@@ -1,3 +1,38 @@
+import { textFileToArray } from '../../utils/textFileToArray.js';
+
+class handShow {
+  constructor(shownRedCubes, shownGreenCubes, shownBlueCubes) {
+    this.shownRedCubes = shownRedCubes;
+    this.shownGreenCubes = shownGreenCubes;
+    this.shownBlueCubes = shownBlueCubes;
+  }
+}
+
+class game {
+  constructor(id, handShows) {
+    this.id = id;
+    this.handShows = handShows;
+  }
+}
+
+const maxRedCubes = 12;
+const maxGreenCubes = 13;
+const maxBlueCubes = 14;
+
+const gamesArray = [];
+
+var idTotal = 0;
+
+function exportGamesToArray(games) {
+  games.forEach(game => {
+    var id;
+    
+  });
+}
+
 export function part1(filePath) {
-    return 0;
+  const games = textFileToArray(filePath);
+  exportGamesToArray(games);
+
+  return idTotal;
 }
